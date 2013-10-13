cc16f0f6fa948ea3425b36fa718123f6ef8226c3
Sarah Whinnem <sarah@betterment.com>
2013-10-13 15:47:20 -0400

    first test woohoo

--- src/tests.rb
+++ src/tests.rb
@@ -1,3 +1,14 @@
 require_relative './todo_list'
 
-# do stuff here
+# when I create a Todo, the title gets assigned
+
+# setup
+todo1 = Todo.new("get milk")
+# action
+todo1.title
+# assertion
+if todo1.title == "get milk"
+    puts "title was assigned"
+else
+    puts "title wasn't assigned - '#{todo1.title}'"
+end
